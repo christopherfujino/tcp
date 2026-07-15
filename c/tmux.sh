fb07180363de1ca5f827b250342a1886fb988504
Christopher Fujino <christopherfujino@gmail.com>
2026-07-15 15:26:29 -0700

    add a tmux.sh script

---
+++ c/tmux.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SESSION='__tcp_c__'
+
+cd "$(dirname $(realpath ${BASH_SOURCE[0]} ) )"
+
+tmux new-session -s "$SESSION" -d
+tmux send-keys -t "$SESSION" 'nvim .' Enter
+# specific to Chris' tmux.conf, on split the focus jumps to new pane
+tmux split-window -h
+tmux send-keys -t "$SESSION" "ipc-runner-server -- 'make'" Enter
+
+# move left
+tmux select-pane -L
+
+exec tmux attach-session -t "$SESSION"
