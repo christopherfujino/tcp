82e7ab9821d585bae78370fa6be47eef8458887a
Christopher Fujino <christopherfujino@gmail.com>
2026-07-23 12:08:18 -0700

    ensure we clean up depfiles

--- tmux.sh
+++ tmux.sh
@@ -10,7 +10,7 @@ tmux new-session -s "$SESSION" -d
 tmux send-keys -t "$SESSION" 'nvim .' Enter
 # specific to Chris' tmux.conf, on split the focus jumps to new pane
 tmux split-window -h
-tmux send-keys -t "$SESSION" "ipc-runner-server -- 'make'" Enter
+tmux send-keys -t "$SESSION" "ipc-runner-server -- './mk.sh'" Enter
 
 # move left
 tmux select-pane -L
